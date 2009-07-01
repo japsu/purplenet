57925f7956d4c453b48c57d98bb83aca680830bd
Santtu Pajukanta <santtu.pajukanta@tut.fi>
2009-07-01 16:23:02 +0300

    moved the main page to /openvpn and login page to /openvpn/login. Django makes changes like this delightfully easy - it was three lines <3

--- openvpnweb/openvpn_userinterface/urls.py
+++ openvpnweb/openvpn_userinterface/urls.py
@@ -6,8 +6,8 @@ from openvpnweb.openvpn_userinterface.views import *
 urlpatterns = patterns('',
 
 #URLs for  user interface
-    url(r'^$', login_page, name="login_page"),
-    url(r'^main/$', main_page, name="main_page"),
+    url(r'^$', main_page, name="main_page"),
+    url(r'^login/$', login_page, name="login_page"),
 #    url(r'^revoke/$', revoke_page, name="revoke_page"),
     url(r'^revoke/(?P<cert_id>\d+)$', revoke_page, name="revoke_page"),
     url(r'^order/(?P<network_id>\d+)$', order_page, name="order_page"),
