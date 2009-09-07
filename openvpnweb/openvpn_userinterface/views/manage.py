16c4aaf5f6b572c255d35c700dba12d74af06b1e
Santtu Pajukanta <santtu.pajukanta@tut.fi>
2009-09-07 17:36:28 +0300

    python 2.5 fix test

--- openvpnweb/openvpn_userinterface/views/manage.py
+++ openvpnweb/openvpn_userinterface/views/manage.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 # vim: shiftwidth=4 expandtab
 
+from __future__ import absolute_import
+
 from ..access_control import manager_required, superuser_required
 from ..forms import *
 from ..models import *
